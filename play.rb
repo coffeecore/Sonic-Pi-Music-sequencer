30562a4e146c041ee0c16baad0c7a80bb615a28c
Antoine G <guillemin.antoine@gmail.com>
2021-01-02 20:08:39 +0100

    Update play.rb

--- play.rb
+++ play.rb
@@ -15,32 +15,31 @@ live_loop :play do
     steps = instru['steps']
     puts "STEPS #{steps}" if get(:debug)
 
-    if steps != nil then
+    if steps[n] != nil then
       opts = instru['opts'].to_h
       puts "OPTS #{opts}" if get(:debug)
       fxs = instru['fxs']
-      if steps[n] != nil then
-        opts['note'] = steps[n]
-        instruName = instru['name']
-        toEval = ''
-        fxs.reverse.each do |fx|
-          toEval += "with_fx :#{fx['name']}, #{fx['opts'].to_h} do "
-        end
-        case instru['type']
-          when 'synth'
-            toEval += "synth instruName.to_sym, opts "
-          when 'sample'
-            toEval += "sample instruName.to_sym, opts "
-          when 'external_sample'
-            toEval += "sample \"#{instruName}\", opts "
-        end
-
-        fxs.reverse.each do |fx|
-          toEval += "end "
-        end
-        puts "STRING #{toEval}" if get(:debug)
-        eval toEval
+      
+      opts['note'] = steps[n]
+      instruName = instru['name']
+      toEval = ''
+      fxs.reverse.each do |fx|
+        toEval += "with_fx :#{fx['name']}, #{fx['opts'].to_h} do "
       end
+      case instru['type']
+        when 'synth'
+          toEval += "synth instruName.to_sym, opts "
+        when 'sample'
+          toEval += "sample instruName.to_sym, opts "
+        when 'external_sample'
+          toEval += "sample \"#{instruName}\", opts "
+      end
+
+      fxs.reverse.each do |fx|
+        toEval += "end "
+      end
+      puts "STRING #{toEval}" if get(:debug)
+      eval toEval
     end
   end
   ##| sleep (1.0/get(:eighth))
