190b5a2b89d5a3ec58255e8166604c3f6d79803a
Antoine Guillemin <guillemin.antoine@gmail.com>
2021-01-07 16:08:53 +0100

    var main

--- main.rb
+++ main.rb
@@ -12,10 +12,10 @@ set :pmax, 1 # Max patterns to play in sequencer mod
 set :p, 0 # Pattern to play in single mod
 set :bpm, 60
 set :volume, 5
-set :debug, true
-set :cue_logging, true
+set :debug, false
+set :cue_logging, false
 set :metronome_state, true
-set :play_state, 1
+set :play_state, 0
 set :sequencer_mod, SEQUENCER_MOD[0]
 set :metronome_options, {
   'release' => 0.001
