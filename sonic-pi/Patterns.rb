b502c7ead5d116e6cebb30ea293eff20cff758a2
Antoine Guillemin <guillemin.antoine@gmail.com>
2021-01-09 21:28:27 +0100

    patterns handler

---
+++ sonic-pi/Patterns.rb
@@ -0,0 +1,44 @@
+live_loop :add_pattern do
+  use_real_time
+  use_cue_logging get(:cue_logging)
+  use_debug get(:debug)
+  osc        = sync '/osc*/instru/pattern/add'
+  instruPos  = osc[0]
+
+  instrus = get(:instrus)[0..]
+
+  instru  = (instrus[instruPos]).to_h
+
+  patterns = instru['patterns'][0..]
+
+  puts "KJKJJKJKJKJKJKJKJKJJKJKKJJKJK"
+  puts patterns
+
+  patterns.push(Array.new(get(:endBar)+1))
+
+  instru['patterns'] = patterns
+
+  instrus[instruPos] = instru
+
+  set(:instrus, instrus)
+end
+
+live_loop :remove_pattern do
+  use_real_time
+  use_cue_logging get(:cue_logging)
+  use_debug get(:debug)
+  osc        = sync '/osc*/instru/pattern/remove'
+  instruPos  = osc[0]
+  patternPos = osc[1]
+
+  instrus = get(:instrus)[0..]
+  instru  = (instrus[instruPos]).to_h
+  patterns   = instru['patterns'][0..]
+
+  patterns.delete_at(patternPos)
+
+  instru['patterns'] = patterns
+
+  instrus[instruPos] = instru
+  set(:instrus, instrus)
+end
