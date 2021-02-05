080cdbf99cdbda0ba46059f91e2aa26d4d5d650d
Antoine Guillemin <guillemin.antoine@gmail.com>
2021-02-05 11:08:09 +0100

    comment record

--- main.rb
+++ main.rb
@@ -142,64 +142,64 @@ end
 # Author : robin.newman
 # URI : https://in-thread.sonic-pi.net/t/recording-is-not-happening-with-osc-commands/4710/6
 
-define :port_value do #get current listen port for Sonic Pi from log file
-  value = 51243 #pre new logfile format port was always 4557
-  File.open(ENV['HOME']+'/.sonic-pi/log/server-output.log','r') do |f1|
-    while l = f1.gets
-      if l.include?"Listen port:"
-        value = l.split(" ").last.to_i
-        break
-      end
-    end
-    f1.close
-  end
-  puts "Port record #{value}"
-  return value
-end
-set :pvalue, port_value
-set :record, false
+# define :port_value do #get current listen port for Sonic Pi from log file
+#   value = 51243 #pre new logfile format port was always 4557
+#   File.open(ENV['HOME']+'/.sonic-pi/log/server-output.log','r') do |f1|
+#     while l = f1.gets
+#       if l.include?"Listen port:"
+#         value = l.split(" ").last.to_i
+#         break
+#       end
+#     end
+#     f1.close
+#   end
+#   puts "Port record #{value}"
+#   return value
+# end
+# set :pvalue, port_value
+# set :record, false
 
-define :record_start do # This command is equivalent to pushing the start recording button
-  use_real_time
-  pvalue = get(:pvalue)
-  osc_send "localhost", pvalue, "/start-recording","guid-rbn"
-  sleep 1 # Make sure recording running before creating any audio to save
-end
+# define :record_start do # This command is equivalent to pushing the start recording button
+#   use_real_time
+#   pvalue = get(:pvalue)
+#   osc_send "localhost", pvalue, "/start-recording","guid-rbn"
+#   sleep 1 # Make sure recording running before creating any audio to save
+# end
 
-define :record_stop do # This command stops a currently recording process
-  use_real_time
-  pvalue = get(:pvalue)
-  osc_send "localhost", pvalue, "/stop-recording","guid-rbn"
-end
+# define :record_stop do # This command stops a currently recording process
+#   use_real_time
+#   pvalue = get(:pvalue)
+#   osc_send "localhost", pvalue, "/stop-recording","guid-rbn"
+# end
 
-define :save_audio do |file|  # This command saves the recorded audio file
-  pvalue = get(:pvalue)
-  osc_send "localhost", pvalue, "/save-recording","guid-rbn",file
-end
+# define :save_audio do |file|  # This command saves the recorded audio file
+#   pvalue = get(:pvalue)
+#   osc_send "localhost", pvalue, "/save-recording","guid-rbn",file
+# end
 
 
-live_loop :start_record do
-    # use_real_time
-    osc = sync '/osc*/record/start'
-    puts "Record starting..."
-    record_start()
-    puts "Record started"
-end
+# live_loop :start_record do
+#     # use_real_time
+#     osc = sync '/osc*/record/start'
+#     puts "Record starting..."
+#     record_start()
+#     puts "Record started"
+# end
 
-live_loop :stop_record do
-    # use_real_time
-    osc = sync '/osc*/record/stop'
-    puts "Record stoping..."
-    record_stop()
-    puts "Record stopped"
-end
+# live_loop :stop_record do
+#     # use_real_time
+#     osc = sync '/osc*/record/stop'
+#     puts "Record stoping..."
+#     record_stop()
+#     puts "Record stopped"
+# end
 
-live_loop :save_record_audio_file do
-    # use_real_time
-    osc = sync '/osc*/record/save'
-    puts "Record saving..."
-    sleep 1
-    save_audio(FILE_PATH+'/records/'+(Time.new).strftime("%Y%m%d_%H%M%S")+'.wav')
-    puts "Record saved"
-    stop
-end
+# live_loop :save_record_audio_file do
+#     # use_real_time
+#     osc = sync '/osc*/record/save'
+#     puts "Record saving..."
+#     sleep 1
+#     save_audio(FILE_PATH+'/records/'+(Time.new).strftime("%Y%m%d_%H%M%S")+'.wav')
+#     puts "Record saved"
+#     stop
+# end
