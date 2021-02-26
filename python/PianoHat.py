950b088a197051907d97dd69643098d03f90f3d9
Antoine Guillemin <guillemin.antoine@gmail.com>
2021-02-26 21:52:47 +0100

    clean

--- python/PianoHat.py
+++ python/PianoHat.py
@@ -18,49 +18,10 @@ class PianoHat:
         self.layout       = self.LAYOUT_CHANNEL
         self.mod          = self.MOD_KEY
         self.notes        = []
-        self.totalChannel = 0
+        self.channel_max  = 0
 
     def get_pattern(self):
         return (self.pattern[0])*7+(self.pattern[1])
 
     def midi_note(self, key: int):
         return (self.octave)*12+key
-
-    # def on_instru_listener(self, channel: int, event: bool):
-    #     # CHANNEL
-    #     if self.layout == self.LAYOUT_CHANNEL:
-    #         if event:
-    #             self.layout = self.LAYOUT_STEP
-    #             # On LED Octave Up
-    #         return
-
-    # def on_octave_up_listener(self, channel:int, event: bool):
-    #     # CHANNEL
-    #     if self.layout == self.LAYOUT_CHANNEL:
-    #         if event:
-    #             if self.totalChannel > 0 and self.channel < self.totalChannel-1:
-    #                 self.channel += 1
-    #             else :
-    #                 self.channel = 0
-    #         return
-
-    # def on_octave_down_listener(self, channel:int, event: bool):
-    #     # CHANNEL
-    #     if self.layout == self.LAYOUT_CHANNEL:
-    #         if event:
-    #             if self.channel > 0:
-    #                 self.channel -= 1
-    #                 return
-    #             if self.totalChannel > 0:
-    #                 self.channel = self.totalChannel-1
-    #         return
-
-    # def on_note_listener(self, key:int, event: bool):
-    #     # CHANNEL
-    #     if self.layout == self.LAYOUT_CHANNEL:
-    #         if event:
-    #             if self.BLACK_KEYS.count(key) != 0:
-    #                 self.pattern[0] = self.BLACK_KEYS.index(key)
-    #             if self.WHITE_KEYS.count(key) != 0:
-    #                 self.pattern[1] = self.WHITE_KEYS.index(key)
-    #         return
